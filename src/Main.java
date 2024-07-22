8c0f70a799b3ab95db36c4268b6ccba676939c7b
fasamii <fasami0512@gmail.com>
2024-07-22 03:45:51 +0200

    less code less functions (i move to trash almost evrything)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import javax.swing.JFrame;
 
-public class Main {
-public static void main(String[] args){
-    
-    SnakeGame game = new SnakeGame();
-    JFrame f = new JFrame();
-    
-    f.setName(game.NAME);
-    f.setSize(game.SCREEN_WIDTH*game.SCALE,game.SCREEN_HEIGHT*game.SCALE);
-    f.setLayout(null);
-    f.setLocationRelativeTo(null);
-    f.add(game);
-    f.setVisible(true);
+public class Main{
 
-}
+    private static int SCREEN_WIDTH = 15;
+    private static int SCREEN_HEIGHT = 10;
+    private static int SIZE = 40;
+
+    public static void main(String[] args) throws Exception{
+        
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(SCREEN_WIDTH * SIZE,SCREEN_HEIGHT * SIZE);
+        f.setLocationRelativeTo(null);
+        f.setResizable(false);
+        
+        SnakeGame game = new SnakeGame(SCREEN_WIDTH, SCREEN_HEIGHT, SIZE);
+
+        f.add(game);
+        f.setVisible(true);
+    }
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,89 +1,42 @@
-import java.util.Random;
-import java.util.Timer;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.Color;
 
-public class SnakeGame extends JLabel implements ActionListener {
+import javax.print.attribute.standard.Sides;
+import javax.swing.JPanel;
+
+public class SnakeGame extends JPanel {
     
-    final String NAME = "Snake Game";
-    final int SCREEN_WIDTH = 15;
-    final int SCREEN_HEIGHT = 10;
-    final int SCALE = 40;
-    private final int DELAY = 100;
-    private final int FOODS_TO_SPAWN = 5;
-    private final Color[] COLOR = {
-        new Color(0x301045),
-        new Color(0x00B050),
-        new Color(0x008040),
-        new Color(0xA01080)
-    };
+        private int WIDTH;
+        private int HEIGHT;
+        private int SIZE;
+        private final Color[] COLOR = {
+            new Color(0x301045),
+            new Color(0x00B050),
+            new Color(0x008040),
+            new Color(0xA01080)
+        };
 
-    private boolean running = true;
-    private int[] snakeX = new int[SCREEN_HEIGHT*SCREEN_WIDTH];
-    private int[] snakeY = new int[SCREEN_HEIGHT*SCREEN_WIDTH];
-    private int[] appleX = new int[FOODS_TO_SPAWN];
-    private int[] appleY = new int[FOODS_TO_SPAWN];
-    private int snake_lenght = 1;
-    private char last_char = ' ';
-    private int foods_spawned = 0;
+        private boolean running = true;
+        private int[] snakeX;
+        private int[] snakeY;
 
-    private final Random rand = new Random();
-    private Timer timer;
-
-    SnakeGame(){
-        this.setName(NAME);
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-        this.setLayout(null);
+    SnakeGame(int WIDTH,int HEIGHT, int SIZE){
+        this.WIDTH=WIDTH;this.HEIGHT=HEIGHT;this.SIZE=SIZE;
+        this.setPreferredSize(new Dimension(this.WIDTH * this.SIZE,this.HEIGHT * this.SIZE));
         this.setBackground(COLOR[0]);
-        this.addKeyListener(new KeyAdapt());
-        this.setVisible(true);
+        snakeX= new int[WIDTH*HEIGHT];
+        snakeY= new int[WIDTH*HEIGHT];
+        this.snakeX[0] = SIZE;this.snakeY[0] = SIZE;
     }
 
-// util methods
-    
-    private void prepare(){
-        snakeX[0] = SCALE;
-        snakeY[0] = SCALE;
-        foods_spawned = 0;
-        last_char = ' ';
-        snake_lenght = 1;
-        running = true;
-        //timer = new Timer(DELAY,this);
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g){
+        g.setColor(COLOR[1]);
+        g.fillRect(this.snakeX[0],this.snakeY[0],SIZE,SIZE);
     }
 
-// actual game
-
-    private void update(){
-
-    }
-
-// =-= Graphics =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   
-    public void paint(Graphics g){
-        
-    }
-
-    private void draw(Graphics g){
-
-    } 
-
-// =-= Action =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-
-    public class KeyAdapt extends KeyAdapter{
-        @Override
-        public void keyPressed(KeyEvent e){
-
-        }
-    }
 }
