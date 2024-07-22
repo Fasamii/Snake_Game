5ec5e433ba5c684eefc118b25af8d6f399f0a717
fasamii <fasami0512@gmail.com>
2024-07-22 03:03:11 +0200

    base for creating snake game (this wont be working ehehe)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+public static void main(String[] args){
+    
+    SnakeGame game = new SnakeGame();
+    JFrame f = new JFrame();
+    
+    f.setName(game.NAME);
+    f.setSize(game.SCREEN_WIDTH*game.SCALE,game.SCREEN_HEIGHT*game.SCALE);
+    f.setLayout(null);
+    f.setLocationRelativeTo(null);
+    f.add(game);
+    f.setVisible(true);
+
+}
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,3 +1,89 @@
-public class SnakeGame {
+import java.util.Random;
+import java.util.Timer;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+public class SnakeGame extends JLabel implements ActionListener {
+    
+    final String NAME = "Snake Game";
+    final int SCREEN_WIDTH = 15;
+    final int SCREEN_HEIGHT = 10;
+    final int SCALE = 40;
+    private final int DELAY = 100;
+    private final int FOODS_TO_SPAWN = 5;
+    private final Color[] COLOR = {
+        new Color(0x301045),
+        new Color(0x00B050),
+        new Color(0x008040),
+        new Color(0xA01080)
+    };
+
+    private boolean running = true;
+    private int[] snakeX = new int[SCREEN_HEIGHT*SCREEN_WIDTH];
+    private int[] snakeY = new int[SCREEN_HEIGHT*SCREEN_WIDTH];
+    private int[] appleX = new int[FOODS_TO_SPAWN];
+    private int[] appleY = new int[FOODS_TO_SPAWN];
+    private int snake_lenght = 1;
+    private char last_char = ' ';
+    private int foods_spawned = 0;
+
+    private final JFrame f = new JFrame();
+    private final Random rand = new Random();
+    private Timer timer;
+
+    SnakeGame(){
+        this.setName(NAME);
+        this.setSize(SCREEN_WIDTH*SCALE,SCREEN_HEIGHT*SCALE);
+        this.setLayout(null);
+        this.setBackground(COLOR[0]);
+        this.addKeyListener(new KeyAdapt());
+        this.setVisible(true);
+    }
+
+// util methods
+    
+    private void prepare(){
+        snakeX[0] = SCALE;
+        snakeY[0] = SCALE;
+        foods_spawned = 0;
+        last_char = ' ';
+        snake_lenght = 1;
+        running = true;
+        //timer = new Timer(DELAY,this);
+    }
+
+// actual game
+
+    private void update(){
+
+    }
+
+// =-= Graphics =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+   
+    private void paint(Graphics g){
+        
+    }
+
+    private void draw(Graphics g){
+
+    } 
+
+// =-= Action =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class KeyAdapt extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
 }
